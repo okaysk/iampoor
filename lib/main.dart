60b9af69be9b35ca4b1d828c54e028d09f73b8e5
sk-weepet <67887045+sk-weepet@users.noreply.github.com>
2020-07-07 13:25:14 +0900

    Stateless Test

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,67 @@
 import 'package:flutter/material.dart';
 
 //The mai function is the starting point for all our Flutter apps.
-void main() {
-  runApp(
-    MaterialApp(
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  int cnt = 0;
+
+  void increase() {
+    cnt++;
+    print(cnt);
+  }
+
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blueGrey[300],
+          title: Text("Stateless Widget"),
+        ),
+        body: Column(
+          children: <Widget>[
+            Center(
+              child: Text("${cnt}"),
+            ),
+            RaisedButton(
+              onPressed: increase,
+              child: Text("Button"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+/*
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+*/
+
+/* 
+  MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.blueGrey,
         appBar: AppBar(
-          title: Text('I am Rich'),
-          backgroundColor: Colors.blueGrey[900],
+          title: Text('Stateless Widget'),
+          backgroundColor: Colors.blueGrey[800],
         ),
-        body: Center(
-          child: Image(
-            image: AssetImage('images/diamond.png'),
+        body: Column(children: <Widget>[
+          Center(
+            child: Text("$cnt"),
           ),
-        ),
+          RaisedButton(
+            onPressed: increase,
+            child: Text("Button"),
+          ),
+        ],
       ),
     ),
   );
-}
+*/
